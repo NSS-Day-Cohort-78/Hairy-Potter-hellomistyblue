c877c793b43ae56d32f543a996f110d07c09dda0
Misty Blue <hellomistyblue@gmail.com>
2025-08-01 18:35:19 -0500

    created PotterWheel and pottery

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,16 @@
+let primaryKey = 1
+
+export const makePottery = (shape, weight, height) => {
+
+
+    return {
+        shape: shape,
+        weight: weight,
+        height: height,
+        id: primaryKey++, 
+
+    } 
+
+}
+
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,24 @@
 // Imports go first
+import { makePottery } from './PotteryWheel.js'
 
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("mug", 1, 3)
+console.log(mug)
+
+let vase = makePottery("vase", 3, 12)
+console.log(vase)
+
+let teapot = makePottery("teapot", 4, 15)
+console.log(teapot)
+
+let pitcher = makePottery("pitcher", 2, 10)
+console.log(pitcher)
+
+let planter = makePottery("planter", 5, 20)
+console.log(planter)
+
 
 
 // Fire each piece of pottery in the kiln
